e3a65c7adcea25a3ebdf935862eaa7b471132a25
Kuvatbekov <kuvatbekoovd3@gmail.com>
2023-12-07 15:27:56 +0600

    dsa

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("asd");
         System.out.println("hello its me Doolot ");
+        System.out.println("asdasd");
     }
 }
